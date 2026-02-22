cee6fc208a7154ba042ca6fe152b67106d75d329
Leonardo Grando <leonardo.grando@unisal.br>
2026-02-22 15:08:09 -0300

    Create exercicio3.violaçãoetipagem.c

---
+++ aula2502/exercicio3.violaçãoetipagem.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int caixa_numero = 10;
+    printf("Valor inicial na memoria: %d\n", caixa_numero);
+    
+    /* INSTRUCAO PARA O ALUNO: 
+     Descomente a linha abaixo, tente compilar com GCC e leia o erro.
+     O compilador nao permite colocar um texto na 'caixa' de inteiros.
+    */
+    
+    // caixa_numero = "Engenharia"; 
+    
+    return 0;
+}
