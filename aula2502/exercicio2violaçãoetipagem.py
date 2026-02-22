349622b2c0ba3bfc3fa8033cd04ec65b215c0d2a
Leonardo Grando <leonardo.grando@unisal.br>
2026-02-22 15:08:57 -0300

    Create exercicio2violaçãoetipagem.py

---
+++ aula2502/exercicio2violaçãoetipagem.py
@@ -0,0 +1,15 @@
+caixa = 10
+print(f"Valor inicial (inteiro): {caixa}")
+
+# Python (tipagem dinamica) permite mudar o formato da 'caixa' em tempo de execucao
+caixa = "Engenharia"
+print(f"Novo valor na mesma variavel (string): {caixa}")
+
+"""
+ INSTRUCAO PARA O ALUNO:
+ Descomente a linha abaixo e rode o script.
+ A tipagem dinamica aceitou a mudanca, mas o sistema falha ao tentar 
+ somar texto com numero, causando um erro em tempo de execucao (Runtime Error).
+"""
+
+# resultado = caixa + 5
